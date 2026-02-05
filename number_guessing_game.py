7dfef8af7134cf228d40c23af69276e56d34ad9b
Faamidha Sulaiman <sn.faamidha@gmail.com>
2026-02-06 00:55:09 +0400

    fixed repeated logic and added an exit option

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,4 +1,43 @@
 import random, sys
+def num_game(max_num):
+    while True:
+            print(f"The number is between 1 to {max_num}.")
+            num=random.randint(1,max_num)
+            count=0
+
+            while True:
+                guess=int(input("Guess the number: "))
+                count+=1
+
+                if guess<=0 or guess>max_num:
+                    print(f"The number is between 1 to {max_num}.")
+
+                elif guess>num:
+                    print("Too high.")
+
+                elif guess<num:
+                    print("Too low.")
+
+                elif guess==num:
+                    print(f"Correct! The number was {num}.")
+                    print(f"You guessed in {count} times.")
+                    break
+
+                else:
+                    print("Guess a number.")
+
+            while True:
+                play=input("Play again? (Y/N): ").lower()
+                if play=="y":
+                    break
+                elif play=="n":
+                    break
+                else:
+                    print("Enter Y or N.")
+
+            if play=="n":
+                break
+
 while True:
     
     print("----Difficulty levels----")
@@ -10,119 +49,13 @@ while True:
     ch=int(input("Enter choice:"))
 
     if ch==1:
-        while True:
-            print("The number is between 1 to 10.")
-            num=random.randint(1,10)
-            count=0
+        num_game(10)
 
-            while True:
-                guess=int(input("Guess the number: "))
-                count+=1
-
-                if guess<=0 or guess>10:
-                    print("The number is between 1 to 10.")
-
-                elif guess>num:
-                    print("Too high.")
-
-                elif guess<num:
-                    print("Too low.")
-
-                elif guess==num:
-                    print(f"Correct! The number was {num}.")
-                    print(f"You guessed in {count} times.")
-                    break
-
-                else:
-                    print("Guess a number.")
-
-            while True:
-                play=input("Play again? (Y/N): ").lower()
-                if play=="y":
-                    break
-                elif play=="n":
-                    break
-                else:
-                    print("Enter Y or N.")
-
-            if play=="n":
-                break
-    elif ch==2:
-        while True:
-            print("The number is between 1 to 50.")
-            num=random.randint(1,50)
-            count=0
-
-            while True:
-                guess=int(input("Guess the number: "))
-                count+=1
-
-                if guess<=0 or guess>50:
-                    print("The number is between 1 to 10.")
-
-                elif guess>num:
-                    print("Too high.")
-
-                elif guess<num:
-                    print("Too low.")
-
-                elif guess==num:
-                    print(f"Correct! The number was {num}.")
-                    print(f"You guessed in {count} times.")
-                    break
-
-                else:
-                    print("Guess a number.")
-
-            while True:
-                play=input("Play again? (Y/N): ").lower()
-                if play=="y":
-                    break
-                elif play=="n":
-                    break
-                else:
-                    print("Enter Y or N.")
-
-            if play=="n":
-                break             
+    elif ch==2:   
+        num_game(50)
 
     elif ch==3:
-        while True:
-            print("The number is between 1 to 100.")
-            num=random.randint(1,100)
-            count=0
+        num_game(100)
 
-            while True:
-                guess=int(input("Guess the number: "))
-                count+=1
-
-                if guess<=0 or guess>100:
-                    print("The number is between 1 to 100.")
-
-                elif guess>num:
-                    print("Too high.")
-
-                elif guess<num:
-                    print("Too low.")
-
-                elif guess==num:
-                    print(f"Correct! The number was {num}.")
-                    print(f"You guessed in {count} times.")
-                    break
-
-                else:
-                    print("Guess a number.")
-
-            while True:
-                play=input("Play again? (Y/N): ").lower()
-                if play=="y":
-                    break
-                elif play=="n":
-                    break
-                else:
-                    print("Enter Y or N.")
-
-            if play=="n":
-                break
-
-
+    else:
+        sys.exit()
